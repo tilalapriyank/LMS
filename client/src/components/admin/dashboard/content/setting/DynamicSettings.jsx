4d809dea64da7cbfbbcdeb1695f67ca0d2f2430b
tilalapriyank <priyankpatel2764@gmail.com>
2025-01-07 12:23:18 +0530

    update s

--- client/src/components/admin/dashboard/content/setting/DynamicSettings.jsx
+++ client/src/components/admin/dashboard/content/setting/DynamicSettings.jsx
@@ -1,207 +1,92 @@
 import React from "react";
 import {
-  Box,
-  TextField,
-  Switch,
-  MenuItem,
+  Grid,
   Typography,
+  TextField,
+  FormControl,
+  Select,
+  MenuItem,
+  Checkbox,
+  FormControlLabel,
+  InputLabel,
   Card,
   CardContent,
-  Grid,
-  FormControlLabel,
-  Checkbox,
 } from "@mui/material";
 
-const DynamicSettings = ({ settings = [], onChange }) => {
-  const renderSetting = (setting) => {
-    const cardStyle = {
-      marginBottom: 2,
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-between",
-      height: "100%", // Ensure all cards have the same height
-    };
-
-    switch (setting.type) {
-      case "number":
-        return (
-          <Grid item md={12} key={setting.id}>
-            <Card sx={cardStyle}>
-              <CardContent>
-                <Typography variant="h6">{setting.title}</Typography>
-                <TextField
-                  type="number"
-                  value={setting.value}
-                  onChange={(e) =>
-                    onChange(setting.id, parseInt(e.target.value, 10))
-                  }
-                  fullWidth
-                  margin="normal"
-                  helperText={setting.description}
-                  inputProps={{
-                    "aria-label": setting.title,
-                  }}
-                />
-              </CardContent>
-            </Card>
-          </Grid>
-        );
-
-      case "switch":
-        return (
-          <Grid item md={12} key={setting.id}>
-            <Card sx={cardStyle}>
-              <CardContent>
-                <Typography variant="h6">{setting.title}</Typography>
-                <Switch
-                  checked={setting.value}
-                  onChange={(e) => onChange(setting.id, e.target.checked)}
-                  inputProps={{
-                    "aria-label": setting.title,
-                  }}
-                />
-                <Typography variant="body2">{setting.description}</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        );
-
-      case "multiple_select":
-        return (
-          <Grid item md={12} key={setting.id}>
-            <Card sx={cardStyle}>
-              <CardContent>
-                <Typography variant="h6">{setting.title}</Typography>
-                <TextField
-                  select
-                  value={setting.value}
-                  onChange={(e) => onChange(setting.id, e.target.value)}
-                  fullWidth
-                  margin="normal"
-                  helperText={setting.description}
-                  inputProps={{
-                    "aria-label": setting.title,
-                  }}
-                  multiple
-                >
-                  {setting.options.map((option) => (
-                    <MenuItem key={option} value={option}>
-                      {option}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </CardContent>
-            </Card>
-          </Grid>
-        );
-
-      case "select":
-        return (
-          <Grid item md={12} key={setting.id}>
-            <Card sx={cardStyle}>
-              <CardContent>
-                <Typography variant="h6">{setting.title}</Typography>
-                <TextField
-                  select
-                  value={setting.value}
-                  onChange={(e) => onChange(setting.id, e.target.value)}
-                  fullWidth
-                  margin="normal"
-                  helperText={setting.description}
-                  inputProps={{
-                    "aria-label": setting.title,
-                  }}
-                >
-                  {setting.options.map((option) => (
-                    <MenuItem key={option} value={option}>
-                      {option}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </CardContent>
-            </Card>
-          </Grid>
-        );
-
-      case "checkbox":
-        return (
-          <Grid item md={12} key={setting.id}>
-            <Card sx={cardStyle}>
-              <CardContent>
-                <Typography variant="h6">{setting.title}</Typography>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={setting.value}
-                      onChange={(e) => onChange(setting.id, e.target.checked)}
-                      inputProps={{
-                        "aria-label": setting.title,
-                      }}
-                    />
-                  }
-                  label={setting.description}
-                />
-              </CardContent>
-            </Card>
-          </Grid>
-        );
-
-      case "color":
-        return (
-          <Grid item md={12} key={setting.id}>
-            <Card sx={cardStyle}>
-              <CardContent>
-                <Typography variant="h6">{setting.title}</Typography>
-                <TextField
-                  type="color"
-                  value={setting.value}
-                  onChange={(e) => onChange(setting.id, e.target.value)}
-                  fullWidth
-                  margin="normal"
-                  helperText={setting.description}
-                  inputProps={{
-                    "aria-label": setting.title,
-                  }}
-                />
-              </CardContent>
-            </Card>
-          </Grid>
-        );
-
-      case "text":
-        return (
-          <Grid item md={12} key={setting.id}>
-            <Card sx={cardStyle}>
-              <CardContent>
-                <Typography variant="h6">{setting.title}</Typography>
-                <TextField
-                  type="text"
-                  value={setting.value}
-                  onChange={(e) => onChange(setting.id, e.target.value)}
-                  fullWidth
-                  margin="normal"
-                  helperText={setting.description}
-                  inputProps={{
-                    "aria-label": setting.title,
-                  }}
-                />
-              </CardContent>
-            </Card>
-          </Grid>
-        );
-
-      default:
-        return null;
-    }
-  };
+const SettingCard = ({ setting, handleInputChange, formValues }) => {
+  const { id, title, description, type, options, value } = setting;
 
   return (
-    <Box>
-      <Grid container spacing={2}>
-        {settings.map(renderSetting)}
-      </Grid>
-    </Box>
+    <Grid item xs={12} sm={6} md={3} key={id}>
+      <Card
+        sx={{
+          mb: 2,
+          borderRadius: 2,
+          boxShadow: 3,
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
+        }}
+      >
+        <CardContent sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" sx={{ mb: 1 }}>
+            {title}
+          </Typography>
+          {description && (
+            <Typography variant="body2" color="textSecondary" sx={{ mb: 2 }}>
+              {description}
+            </Typography>
+          )}
+          {type === "text" && (
+            <TextField
+              variant="outlined"
+              fullWidth
+              defaultValue={value}
+              onChange={(e) => handleInputChange(id, e.target.value)}
+            />
+          )}
+          {type === "number" && (
+            <TextField
+              label={title}
+              type="number"
+              variant="outlined"
+              fullWidth
+              defaultValue={value}
+              onChange={(e) => handleInputChange(id, e.target.value)}
+            />
+          )}
+          {type === "switch" && (
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={value}
+                  onChange={(e) => handleInputChange(id, e.target.checked)}
+                  name={id}
+                />
+              }
+              label={title}
+            />
+          )}
+          {type === "select" && (
+            <FormControl fullWidth>
+              <InputLabel>{title}</InputLabel>
+              <Select
+                label={title}
+                value={value}
+                onChange={(e) => handleInputChange(id, e.target.value)}
+              >
+                {options?.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          )}
+        </CardContent>
+      </Card>
+    </Grid>
   );
 };
 
-export default DynamicSettings;
+export default SettingCard;
--- client/src/components/admin/dashboard/content/setting/Setting.jsx
+++ client/src/components/admin/dashboard/content/setting/Setting.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import { Box, Tab, Tabs, Typography, TextField, FormControl, Select, MenuItem, Checkbox, FormControlLabel, InputLabel, Card, CardContent, Grid } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Box, Tab, Tabs, Typography, Grid, Button } from "@mui/material";
 import { settingsTabs } from "../../../../../utils/setting";
+import SettingCard from "./DynamicSettings";
 
 const SettingsPageHorizontalTabs = () => {
   const [selectedTab, setSelectedTab] = useState(0);
@@ -11,95 +12,63 @@ const SettingsPageHorizontalTabs = () => {
   };
 
   const handleInputChange = (id, value) => {
-    setFormValues(prevValues => ({
+    setFormValues((prevValues) => ({
       ...prevValues,
-      [id]: value
+      [selectedTab]: {
+        ...prevValues[selectedTab],
+        [id]: value,
+      },
     }));
   };
 
-  const renderSettingField = (setting) => {
-    const { id, title, description, type, options, value } = setting;
-
-    const settingField = (
-      <Grid item xs={12} sm={6} md={4} key={id}>
-        <Card sx={{ mb: 2, borderRadius: 2, boxShadow: 3, display: 'flex', flexDirection: 'column', height: '100%' }}>
-          <CardContent sx={{ flexGrow: 1 }}>
-            <Typography variant="h6" sx={{ mb: 1 }}>
-              {title}
-            </Typography>
-            {description && (
-              <Typography variant="body2" color="textSecondary" sx={{ mb: 2 }}>
-                {description}
-              </Typography>
-            )}
-            {type === "text" && (
-              <TextField
-                variant="outlined"
-                fullWidth
-                defaultValue={value}
-                onChange={(e) => handleInputChange(id, e.target.value)}
-              />
-            )}
-            {type === "number" && (
-              <TextField
-                label={title}
-                type="number"
-                variant="outlined"
-                fullWidth
-                defaultValue={value}
-                onChange={(e) => handleInputChange(id, e.target.value)}
-              />
-            )}
-            {type === "switch" && (
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={value}
-                    onChange={(e) => handleInputChange(id, e.target.checked)}
-                    name={id}
-                  />
-                }
-                label={title}
-              />
-            )}
-            {type === "select" && (
-              <FormControl fullWidth>
-                <InputLabel>{title}</InputLabel>
-                <Select
-                  label={title}
-                  value={value}
-                  onChange={(e) => handleInputChange(id, e.target.value)}
-                >
-                  {options?.map((option) => (
-                    <MenuItem key={option} value={option}>
-                      {option}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            )}
-          </CardContent>
-        </Card>
-      </Grid>
-    );
-
-    return settingField;
+  const handleSave = () => {
+    console.log("Saving settings for tab:", settingsTabs[selectedTab].label);
+    console.log("Form Values: ", formValues[selectedTab] || {});
   };
+
+  useEffect(() => {
+    if (!formValues[selectedTab]) {
+      setFormValues((prevValues) => ({
+        ...prevValues,
+        [selectedTab]: settingsTabs[selectedTab]?.settings.reduce(
+          (acc, setting) => ({
+            ...acc,
+            [setting.id]: setting.value || "",
+          }),
+          {}
+        ),
+      }));
+    }
+  }, [selectedTab, formValues]);
 
   return (
     <Box sx={{ padding: 3 }}>
-      <Tabs value={selectedTab} onChange={handleTabChange} aria-label="Horizontal tabs example">
+      <Tabs
+        value={selectedTab}
+        onChange={handleTabChange}
+        aria-label="Horizontal tabs example"
+      >
         {settingsTabs.map((tab, index) => (
           <Tab label={tab.label} key={tab.name} />
         ))}
       </Tabs>
       <Box sx={{ marginTop: 3 }}>
-        <Typography variant="h5" sx={{ mb: 3, fontWeight: 'bold' }}>
+        <Typography variant="h5" sx={{ mb: 3, fontWeight: "bold" }}>
           {settingsTabs[selectedTab]?.label}
         </Typography>
         <Grid container spacing={3}>
-          {settingsTabs[selectedTab]?.settings.map((setting) => renderSettingField(setting))}
+          {settingsTabs[selectedTab]?.settings.map((setting) => (
+            <SettingCard
+              key={setting.id}
+              setting={setting}
+              handleInputChange={handleInputChange}
+              formValues={formValues[selectedTab] || {}}
+            />
+          ))}
         </Grid>
+        <Button variant="contained" sx={{ mt: 3 }} onClick={handleSave}>
+          Save
+        </Button>
       </Box>
     </Box>
   );
